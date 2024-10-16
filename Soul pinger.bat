5c49a3c1943edf69fd8b5fef782cabf9b2445d17
Upsetting <xenex.cfx@gmail.com>
2024-10-16 22:21:47 +0100

    Add files via upload

---
+++ Soul pinger.bat
@@ -0,0 +1,114 @@
+@echo off
+:a
+mode 100,50
+color C
+
+title Upsetting Ip pinger!
+
+:greeting
+cls  
+                                                                     
+echo           #-----------------# #-----------#
+echo           # @Upsetting # # IP PINGER #
+echo           #-----------------# #-----------#
+echo !Name doesnt get logged just for a better experience!
+set /p Name= Enter A Name:
+title Oi %Name% Target=
+set /p IP= Smack Ip here %Name%:
+title Oi %Name% Target= %IP%
+echo Target = %IP%
+echo RIP WIFI ----%IP% -- Upsetting
+title WiFi eat shit nerd
+:top
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been taken down...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 01
+echo -- %Name% OP --
+title U
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+echo -- %Name% OP --
+title Up
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+echo -- %Name% OP --
+title Ups
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+echo -- %Name% OP --
+title Upse
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+echo -- %Name% OP --
+title Upsett
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+echo -- %Name% OP --
+title Upsetti
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+echo -- %Name% OP --
+title Upsetting
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+echo -- %Name% OP --
+title Upsetting T
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+echo -- %Name% OP --
+title Upsetting To
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+echo -- %Name% OP --
+title Upsetting Too
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+echo -- %Name% OP --
+title Upsetting Too
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+echo -- %Name% OP --
+title Upsetting Too O
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+echo -- %Name% OP --
+title Upsetting Too OP
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+echo -- %Name% OP --
+title Upsetting Too OP Lol
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo fat cunt %IP% has been raped by %Name%...)
+ping -t 2 0 10 127.0.0.1 >nul
+echo -- %Name% OP --
+title Get fucked
+echo Made by @Upsetting
+echo %Name% going for %IP% whos gonna get fucked
+GoTo top
